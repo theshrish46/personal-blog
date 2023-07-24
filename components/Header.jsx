576ef6e5ecfbd5bdc111f5849027a8553cd13a01
theshrish46 <theshrish46@gmail.com>
2023-07-24 17:08:26 +0530

    worked on prototype

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,14 +5,14 @@ import { BsPersonFill, BsSunFill } from 'react-icons/bs';
 const Header = () => {
   return (
     <>
-      <div className='px-4 my-4 w-full flex justify-between items-center'>
+      <div className='px-4 my-4 w-8/12 mx-auto flex justify-between items-center text-xl font-semibold'>
         <Link href={'/'}>Shrish</Link>
 
         <div className='flex justify-center items-center'>
           <Link href={'/login'} className='text-black mr-4'>
             <BsPersonFill />
           </Link>
-          <BsSunFill className='ml-4'/>
+          <BsSunFill className='ml-4' />
         </div>
       </div>
     </>
--- server/index.js
+++ server/index.js
@@ -1,23 +1,29 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+
 const app = express();
 
 // Importing Routers
-const { userAgent } = require('next/server');
-const userAuth = require('./routes/userAuth');
-// const loginpage = require('./routes/loginpage');
+// const userAuth = require('./routes/userAuth');
 
 // Middlewears
 app.use(express.json());
 app.use(cors());
 
-app.use('/login', userAuth);
+// app.use('/login', userAuth);
 // app.use('/login-page', loginpage);
 
-app.post('/loginpage', (req, res) => {
-  const reqBody = req.body;
-  console.log(reqBody);
-  res.send(200).json({ name: 'OK' });
+app.post('/register', async (req, res) => {
+  const { userName, password } = req.body;
+  console.log(userName);
+  res.json({ name: userName, pass: password });
+});
+
+app.put('/login', async (req, res) => {
+  const { userName, password } = req.body;
+  console.log(userName, password);
+  res.json({ name: userName, password: password });
 });
 
 app.listen(8000, () => {
