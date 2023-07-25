455f324e897dfa657e9003a3ac0bed9c3491597d
theshrish46 <theshrish46@gmail.com>
2023-07-25 20:22:58 +0530

    added auth pages

--- backend/server.js
+++ backend/server.js
@@ -13,4 +13,18 @@ app.get('/', (req, res) => {
   res.status(200).json({ message: 'Ok' });
 });
 
+app.post('/loginpage', (req, res) => {
+  const { email, password } = req.body;
+
+  res.status(200).json({ email: email, password: password });
+});
+
+app.post('/user-register', (req, res) => {
+  const { userName, userEmail, password } = req.body;
+
+  res
+    .status(200)
+    .json({ name: userName, email: userEmail, password: password });
+});
+
 app.listen(PORT);
